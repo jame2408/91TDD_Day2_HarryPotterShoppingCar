43641095f7a7d75225f11a67db75413d46f26539
cs4171 <cs4171@SKCS4171>
2017-06-19 13:22:19 +0800

    unit test : 「第一集買了一本_其他都沒買_價格應為_100元」（紅燈）

---
+++ HarryPotterShoppingCar/HarryPotterBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HarryPotterShoppingCar
+{
+    internal class HarryPotterBook
+    {
+        private IList<int> list;
+
+        internal HarryPotterBook(IList<int> list)
+        {
+            this.list = list;
+        }
+
+        internal double BuyBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HarryPotterShoppingCar/HarryPotterBookTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace HarryPotterShoppingCar
+{
+    [TestClass]
+    public class HarryPotterBookTest
+    {
+        [TestMethod]
+        public void 第一集買了一本_其他都沒買_價格應為_100元()
+        {
+            //arrange 
+            var target = new HarryPotterBook(new List<int> { 1, 0, 0, 0, 0 });
+
+            //act 
+            double actual = target.BuyBooks();
+
+            //assert 
+            Assert.AreEqual(100d, actual);
+        }
+    }
+}
